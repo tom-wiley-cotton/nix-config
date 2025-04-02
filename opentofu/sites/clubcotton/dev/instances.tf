357a97245f37e0c098b08afd292a257d9dabdbc4
Bob Cotton <bob.cotton@gmail.com>
2025-04-01 21:00:39 -0600

    haos image and profile
    along with maclan support

--- opentofu/sites/clubcotton/dev/instances.tf
+++ opentofu/sites/clubcotton/dev/instances.tf
@@ -32,3 +32,43 @@ resource "incus_instance" "test_container" {
     }
   }
 }
+
+# Home Assistant OS instance with bridged networking
+resource "incus_instance" "homeassistant" {
+  name      = "homeassistant"
+  image     = "haos"
+  type      = "virtual-machine"
+  profiles  = ["haos"]
+  project   = "default"
+
+  device {
+    name = "eth0"
+    type = "nic"
+
+    properties = {
+      nictype = "bridged"
+      parent  = var.network_bridge
+      hwaddr  = "00:16:3e:3d:95:f0"
+    }
+  }
+}
+
+# Test Home Assistant OS instance with macvlan networking
+resource "incus_instance" "homeassistant_macvlan" {
+  name      = "homeassistant-macvlan"
+  image     = "haos"
+  type      = "virtual-machine"
+  profiles  = ["haos-macvlan"]
+  project   = "default"
+
+  device {
+    name = "eth0"
+    type = "nic"
+
+    properties = {
+      nictype = "macvlan"
+      parent  = var.host_interface
+      hwaddr  = "00:16:3e:3d:95:f1"  # Different MAC address from the bridged instance
+    }
+  }
+}
--- opentofu/sites/clubcotton/dev/profiles.tf
+++ opentofu/sites/clubcotton/dev/profiles.tf
@@ -83,3 +83,71 @@ resource "incus_profile" "container" {
     }
   }
 }
+
+# Profile for Home Assistant OS with bridged networking
+resource "incus_profile" "haos" {
+  name = "haos"
+
+  config = {
+    "limits.cpu"          = "2"
+    "limits.memory"       = "4GB"
+    "security.secureboot" = "false"
+    "boot.autostart"      = "true"
+    "migration.stateful"  = "true"
+  }
+
+  device {
+    name = "eth0"
+    type = "nic"
+    
+    properties = {
+      nictype = "bridged"
+      parent  = var.network_bridge
+    }
+  }
+
+  device {
+    name = "root"
+    type = "disk"
+    
+    properties = {
+      pool = var.storage_pool
+      path = "/"
+      size = "64GB"
+    }
+  }
+}
+
+# Profile for Home Assistant OS with macvlan networking
+resource "incus_profile" "haos_macvlan" {
+  name = "haos-macvlan"
+
+  config = {
+    "limits.cpu"          = "2"
+    "limits.memory"       = "4GB"
+    "security.secureboot" = "false"
+    "boot.autostart"      = "true"
+    "migration.stateful"  = "true"
+  }
+
+  device {
+    name = "eth0"
+    type = "nic"
+    
+    properties = {
+      nictype = "macvlan"
+      parent  = var.host_interface
+    }
+  }
+
+  device {
+    name = "root"
+    type = "disk"
+    
+    properties = {
+      pool = var.storage_pool
+      path = "/"
+      size = "64GB"
+    }
+  }
+}
