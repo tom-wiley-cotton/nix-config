533c5f4fe2189f50f1bd054f8744ee17618d4b48
Bob Cotton <bob.cotton@gmail.com>
2025-04-02 21:46:28 -0600

    added condo and some docs

---
+++ opentofu/sites/condo/prod/variables.tf
@@ -0,0 +1,48 @@
+variable "network_bridge" {
+  description = "Name of the network bridge to use"
+  type        = string
+  default     = "br0"
+}
+
+variable "storage_pool" {
+  description = "Name of the storage pool to use"
+  type        = string
+  default     = "local"
+}
+
+variable "vm_config" {
+  description = "Default configuration for virtual machines"
+  type        = map(string)
+  default     = {
+    "limits.cpu"          = "4"
+    "limits.memory"       = "8GB"
+    "security.secureboot" = "false"
+    "boot.autostart"      = "true"
+  }
+}
+
+variable "storage_sizes" {
+  description = "Storage sizes for different instance types"
+  type        = map(string)
+  default     = {
+    "vm" = "32GB"
+  }
+}
+
+variable "base_config" {
+  description = "Base configuration for all instances"
+  type = object({
+    limits_cpu    = string
+    limits_memory = string
+  })
+  default = {
+    limits_cpu    = "2"
+    limits_memory = "2GB"
+  }
+}
+
+variable "environment" {
+  description = "Environment name (dev/prod)"
+  type        = string
+  default     = "prod"
+}
