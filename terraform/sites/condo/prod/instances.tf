0129f989d6f621008c92b865d31e5a7a74938602
Bob Cotton <bob.cotton@gmail.com>
2025-04-02 22:05:45 -0600

    tf fixes

--- terraform/sites/condo/prod/instances.tf
+++ terraform/sites/condo/prod/instances.tf
@@ -1,10 +1,12 @@
 # Home Assistant OS instance with bridged networking
 resource "incus_instance" "homeassistant" {
   name      = "${var.environment}-homeassistant"
-  image     = "haos"
+  image     = incus_image.haos.aliases[0]
   type      = "virtual-machine"
   profiles  = [module.profiles.haos_profile_name]
   project   = "default"
+
+  depends_on = [incus_image.haos]
 
   device {
     name = "eth0"
