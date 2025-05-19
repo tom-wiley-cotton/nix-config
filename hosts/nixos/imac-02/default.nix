9f085aa9b54448f5080c0e16827066d8ffe8f043
Bob Cotton <bob.cotton@gmail.com>
2025-05-19 13:06:01 -0600

    move imac-02 to ethernet

--- hosts/nixos/imac-02/default.nix
+++ hosts/nixos/imac-02/default.nix
@@ -51,27 +51,18 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking = {
-    useDHCP = false;
-    interfaces = {
-      wlp3s0.useDHCP = true;
-      enp4s0f0.useDHCP = false;
-    };
     hostName = "imac-02";
-    # head -c4 /dev/urandom | od -A none -t x4
     hostId = "95c41ddc";
-    defaultGateway = {
-      interface = "wlp3s0";
-      address = "192.168.5.1";
-    };
+
+    useDHCP = false;
+    defaultGateway = "192.168.5.1";
     nameservers = ["192.168.5.220"];
-    wireless.enable = true;
-    wireless.userControlled.enable = true;
-    wireless.secretsFile = config.age.secrets.wireless-config.path;
-    wireless.networks = {
-      "clubcotton2" = {
-        pskRaw = "ext:PSK";
-      };
-    };
+    interfaces.enp4s0f0.ipv4.addresses = [
+      {
+        address = "192.168.5.153";
+        prefixLength = 24;
+      }
+    ];
   };
 
   # Set your time zone.
