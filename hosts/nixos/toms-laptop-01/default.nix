1b4d4118299895bf1b90ffcc7d8b1c383068266d
wiley-cotton <thomaswileycotton@gmail.com>
2025-08-01 19:42:28 -0600

    new laptop config

---
+++ hosts/nixos/toms-laptop-01/default.nix
@@ -0,0 +1,90 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  config,
+  pkgs,
+  lib,
+  unstablePkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../../modules/node-exporter
+    ../../../modules/nfs
+  ];
+
+  services.clubcotton = {
+    scanner.enable = true;
+    tailscale.enable = true;
+  };
+
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    dockerSocket.enable = true;
+    # Required for containers under podman-compose to be able to talk to each other.
+    defaultNetwork.settings.dns_enabled = true;
+  };
+  virtualisation.libvirtd.enable = true;
+
+  clubcotton.zfs_single_root = {
+    enable = true;
+    poolname = "rpool";
+    swapSize = "4G";
+    disk = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S59CNM0R836896T";
+    useStandardRootFilesystems = true;
+    reservedSize = "20GiB";
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    hostName = "toms-laptop-01";
+    # hostId = "95c41ddc";
+
+    useDHCP = false;
+    defaultGateway = "192.168.5.1";
+    nameservers = ["192.168.5.220"];
+    interfaces.qlp0s20f0u1.ipv4.addresses = [
+      {
+        address = "192.168.5.16";
+        prefixLength = 24;
+      }
+    ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  programs.zsh.enable = true;
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKW08oClThlF1YJ+ey3y8XKm9yX/45EtaM/W7hx5Yvzb tomcotton@Toms-MacBook-Pro.local"
+    ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  networking.firewall.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    vscode
+  ];
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
