359fc8c67eb793ad5e3cbb2b2fa238c9a38dffd9
wileycotton <thomaswileycotton@gmail.com>
2025-08-20 20:37:07 -0600

    needs rekeying

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -18,6 +18,7 @@ let
   nas-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4/K8BbFVT/V5SRlWwjBb2vowBQjCiReOeNRw+C+/c4 root@nas-01";
   octoprint = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtxU4yWKvKtZUV82nISi21UCnZ8D2ua8mPMkhk1flNH root@octoprint";
   frigate-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7E24JIWthIHIyTnqjdmJPeGUw8UreinxDNfVq9N2AP root@frigate-host";
+  toms-laptop-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDTtjWF4ZxB9xIcJeOPpGE7swaikFG52fSJQmIz4sQuE root@toms-laptop-01";
 
   systems = [admin condo-01 natalya-01 dns-01 imac-01 imac-02 nix-01 nix-02 nix-03 nas-01 octoprint frigate-host];
 in {
