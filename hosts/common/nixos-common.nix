d459e5694671905963606e2511060f1d97d6063e
wiley-cotton <thomaswileycotton@gmail.com>
2025-08-21 09:12:40 -0600

    starting cosmic desktop env

--- hosts/common/nixos-common.nix
+++ hosts/common/nixos-common.nix
@@ -22,6 +22,16 @@ in {
     };
   };
 
+  modules = [
+    {
+      nix.settings = {
+        substituters = [ "https://cosmic.cachix.org/" ];
+        trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+      };
+    }
+    nixos-cosmic.nixosModules.default
+  ];
+
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
