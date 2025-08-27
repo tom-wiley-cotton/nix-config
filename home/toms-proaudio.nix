85d81b20843a532e05405a4d25de4c0a8c127a21
wileycotton <thomaswileycotton@gmail.com>
2025-08-26 18:41:46 -0600

    musnix added to flake

---
+++ home/toms-proaudio.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  unstablePkgs,
+  lib,
+  inputs,
+  ...
+}: let
+  inherit (inputs) nixpkgs nixpkgs-unstable;
+in {
+  environment.systemPackages = with pkgs; [
+    pipewire
+    wireplumber
+    
+  ];
+}
