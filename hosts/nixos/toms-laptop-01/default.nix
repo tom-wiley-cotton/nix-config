0049e73b228c20277166002d206ed36e0fe38e30
wiley-cotton <thomaswileycotton@gmail.com>
2025-08-27 16:02:38 -0600

    removed some user group stuff

--- hosts/nixos/toms-laptop-01/default.nix
+++ hosts/nixos/toms-laptop-01/default.nix
@@ -26,7 +26,7 @@
 
   # === Audio Friendly Kernel Mods ===
   musnix.enable = true;
-  users.users.tomcotton.extraGroups = [ "audio" "input" "uinput" ];
+  users.users.tomcotton.extraGroups = [ "audio" ];
   boot.kernelPackages = pkgs.linuxPackages-rt;
 
   services.xserver.enable = true;
