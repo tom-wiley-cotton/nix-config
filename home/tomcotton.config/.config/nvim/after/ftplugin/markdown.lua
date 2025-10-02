176b7c5f69e421a261a4f26e8860f98e6a8da86e
wiley-cotton <thomaswileycotton@gmail.com>
2025-10-02 17:46:45 -0600

    some writing stuff

--- home/tomcotton.config/.config/nvim/after/ftplugin/markdown.lua
+++ home/tomcotton.config/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,9 @@
+im.fn['pencil#init']()
 -- Set these keymaps only for the current buffer
 -- vim.keymap.set with buffer = 0 makes the mapping local to this file
 vim.keymap.set('n', 'j', 'gj', { buffer = 0, noremap = true })
 vim.keymap.set('n', 'k', 'gk', { buffer = 0, noremap = true })
-vim.opt_local.wrap = true
 
 vim.fn['pencil#init']()
+
+vim.opt.textwidth = 80
--- home/tomcotton.config/.config/nvim/after/ftplugin/tex.lua
+++ home/tomcotton.config/.config/nvim/after/ftplugin/tex.lua
@@ -4,3 +4,5 @@ vim.keymap.set('n', 'j', 'gj', { buffer = 0, noremap = true })
 vim.keymap.set('n', 'k', 'gk', { buffer = 0, noremap = true })
 
 vim.fn['pencil#init']()
+
+vim.opt.textwidth = 80
